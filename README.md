06697e47f8cc238dc6c065d456e2f3d2dc5cb0ee
shunichi <shunichi@omise.co>
2018-10-24 10:41:34 +0900

    working aws lambda code (#1)
    * initial commit
    
    * updated readme
    
    * unified quotation
    
    * fixed README
    
    * formatted code
    
    * removed mocha line since not being used
    
    * wip woriking with request-promise
    
    * refactored using request-promise
    
    * added filter on months_til_expiration
    
    * refactored and added lodash
    
    * refactored using lodash
    
    * simplified code
    
    * added filter for notified cards
    
    * removed unnecessary await
    
    * send card ids with status
    
    * added ability to register notified cards
    
    * handle case when notification is not needed
    
    * replaced function with const
    
    * code cleansing
    
    * variable name fix
    
    * changed var to let
    
    * changed variable name to a more apropriate one
    
    * [wip] fixes
    
    * renamed reportBack to markAsDonePromises
    
    * fixed some stuff
    
    * replaced let with const where possible
    
    * do as much as possible initially
    
    * refactored code
    
    * refactored some more
    
    * changed forEach to map since it is more reliable
    
    * fixed bug where card id was not set properly
    
    * squeezed months_til_expiration inside card hash
    
    * fixed bugs
    
    * reinitialize node module, upgrade eslint and their config, add `lint` command
    
    * fixed formatting
    
    * deleted unnecessary space
    
    * avoid mutation
    
    * fixed unnecessary let to const
    
    * avoid returning nothing
    
    * simplified templateParams creation code
    
    * added context.fail procedure in case of failure
    
    * reworded function name
    
    * changed notification determination formula from < to <=
    
    * made mail source configurable

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# card-expiration-notifier
-aws lambda code to check and notify expiring cards
+# Omise card expiration notifier
+A sample AWS lambda code to check and notify expiring cards
+
+## Setup
+1. install dependencies: 
+
+   ```bash
+   npm install
+   ```
+
+2. compress this directory: 
+
+   ```bash
+   zip -r ../card-expiration.zip *
+   ```
+
+3. Upload compressed code created in the previous step to AWS lambda
+
+4. Make an email template using keywords: `name`, `month`, `year`, `months_til_expiration`, `card_id` save it (overwrite it) to `notification_email_template.json` and upload it to AWS.
+
+   ```bash
+   aws ses create-template --cli-input-json file://notification_email_template.json
+   ```
+
+5. Set environment variable `OMISE_SECRET_KEY` to your secret key
+6. Change `Source` in `createEmailParam` and `remaining_months_threshold` in `extractExpiringCards` in `index.js` appropriately.
+
+
+
+## Notes
+
+- After a card is notified of it's expiration, the card ID is registered in the owners (account that the card is associated with) metadata by a key named `notified_expiration`. This key's value is an array so the newly notified card ID would be appended to this array. When updating a card you have to clear it from this array to enable monitoring for expiration again.
+- AWS SES allows a maximum of 50 address to send at a time. Therefore if the number of cards to notify exceeds this number, you must change the code or run this code multiple times.
+- The sample email in this repo `notification_email_template.json` might not be suitable as a notification email. You should customize it before registering it on AWS SES.
---
+++ index.js
@@ -0,0 +1,149 @@
+const rp = require('request-promise')
+const aws = require('aws-sdk')
+const _ = require('lodash')
+
+const Config = {
+  AWSRegion: process.env.SES_AWS_REGION || 'us-east-1',
+  RemainingMonthsThreshold: parseInt(process.env.REMAINING_MONTHS_THRESHOLD || 2),
+  OmiseSecretKey: process.env.OMISE_SECRET_KEY || '',
+  OmiseCustomerApiUrl: process.env.OMISE_CUSTOMER_API_URL || 'https://api.omise.co/customers',
+  SesSource: process.env.SES_SOURCE || ''
+}
+
+const ses = new aws.SES({
+  region: Config.AWSRegion
+})
+
+const monthDiff = (month, year) => {
+  const today = new Date()
+  return (year - today.getFullYear()) * 12 + month - today.getMonth()
+}
+
+const selectUnnotifiedCards = (customer, cards) => {
+  const alreadyNotified = _.get(customer, 'metadata.notified')
+  if (alreadyNotified) {
+    return _.filter(cards, c => !alreadyNotified.includes(c.id))
+  } else {
+    return cards
+  }
+}
+
+const makeEmailParamsPerCard = (customerEmail, card) => {
+  const templateParams = {
+    id: card.id,
+    name: card.name,
+    month: card.expiration_month,
+    year: card.expiration_year,
+    months_til_expiration: card.months_til_expiration
+  }
+
+  return {
+    Destination: { ToAddresses: [customerEmail] },
+    ReplacementTemplateData: JSON.stringify(templateParams)
+  }
+}
+
+const customerWithEmailParams = (customer) => {
+  const unnotifiedCards = selectUnnotifiedCards(customer, customer.cards.data)
+  const cardsWithExpiration = _.map(unnotifiedCards, c => _.merge(c, Object({ months_til_expiration: monthDiff(c.expiration_month, c.expiration_year) })))
+  const cardsToNotify = _.filter(cardsWithExpiration, c => c.months_til_expiration <= Config.RemainingMonthsThreshold)
+
+  return {
+    emailParams: _.map(cardsToNotify, c => makeEmailParamsPerCard(customer.email, c)),
+    customer_id: customer.id,
+    card_ids: _.map(cardsToNotify, c => c.id),
+    notified: _.get(customer, 'metadata.notified', [])
+  }
+}
+
+const getCustomers = async () => {
+  const getCustomersParams = {
+    url: Config.OmiseCustomerApiUrl,
+    auth: {
+      user: Config.OmiseSecretKey,
+      pass: ''
+    },
+    transform: function (body) {
+      return _.map(JSON.parse(body).data, c => customerWithEmailParams(c))
+    }
+  }
+  return rp(getCustomersParams)
+}
+
+const createBulkEmailParams = (destinations) => {
+  return {
+    Destinations: destinations,
+    Source: Config.SesSource,
+    Template: 'ExpiredCardNotification',
+    DefaultTemplateData: '{"name":"","month":"","year":"","months_til_expiration":"","card_id":""}'
+  }
+}
+
+const registerNotified = async (customerId, cardIds) => {
+  const data = {
+    metadata: {
+      notified: cardIds
+    }
+  }
+  const patchCustomersParams = {
+    url: Config.OmiseCustomerApiUrl + '/' + customerId,
+    method: 'PATCH',
+    body: data,
+    json: true,
+    auth: {
+      'user': process.env.OMISE_SECRET_KEY,
+      'pass': ''
+    }
+  }
+  return rp(patchCustomersParams)
+}
+
+const markAsNotifiedPromises = (cardStatus, customers) => {
+  return _.map(customers, function (customer) {
+    const notified = customer.notified
+    const cardIds = _.filter(customer.card_ids, c => cardStatus[c] === true)
+    const newNotified = notified.concat(cardIds)
+    const customerId = customer.customer_id
+    return registerNotified(customerId, newNotified)
+  })
+}
+
+const main = async () => {
+  const customers = await getCustomers()
+  const toSend = _.filter(customers, c => c.card_ids.length > 0)
+  const destinations = _.flatMap(toSend, c => c.emailParams)
+  const cards = _.flatMap(toSend, c => c.card_ids)
+
+  if (cards.length === 0) {
+    return { cardStatus: [], customers: [] }
+  }
+
+  console.log('===SENDING EMAIL===')
+  const bulkEmailParams = createBulkEmailParams(destinations)
+  const sendPromise = ses.sendBulkTemplatedEmail(bulkEmailParams).promise()
+  return sendPromise.then((data) => {
+    const statuses = {}
+    _.zip(cards, data.Status).forEach(function ([cardId, status]) {
+      statuses[cardId] = status.Status === 'Success'
+    })
+    return { cardStatus: statuses, customers: toSend }
+  })
+}
+
+exports.handler = async (event, context) => {
+  console.log('Incoming: ', event)
+
+  const result = await main()
+    .catch(err => {
+      console.log('error', err)
+      return context.fail(err)
+    })
+  if (result.customers.length === 0) {
+    return context.succeed(event)
+  }
+  console.log(result.cardStatus)
+  console.log('===EMAIL SENT===')
+  const reports = await markAsNotifiedPromises(result.cardStatus, result.customers)
+  await Promise.all(reports)
+  context.succeed(event)
+}
